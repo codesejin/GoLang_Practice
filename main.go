9218d23d29832f55203d4ed07054c76e0d3ee35b
codesejin <seijin0722@naver.com>
2023-02-07 19:55:43 +0900

    url checker

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
-	"codesejin/golearn/mydict"
+	"errors"
 	"fmt"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request failed")
+
 func main() {
-	dictionary := mydict.Dictionary{}
-	basWord := "hello"
-	dictionary.Add(basWord, "First")
-	dictionary.Search(basWord)
-	dictionary.Delete(basWord)
-	word, err := dictionary.Search(basWord)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word, err)
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+// error handle logic
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
