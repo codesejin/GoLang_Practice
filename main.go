b51b280e3500469dc19dbd89f9171de5cb66a5e7
codesejin <seijin0722@naver.com>
2023-02-07 20:53:24 +0900

    channels Recap

--- main.go
+++ main.go
@@ -7,19 +7,18 @@ import (
 
 // Channel 은 goroutine이랑 메인함수 사이에 정보를 전달하기 위한 방법
 func main() {
-	c := make(chan bool)
-	people := [2]string{"nico", "flynn"}
+	c := make(chan string)
+	people := [5]string{"nico", "flynn", "sejin", "mommy", "larry"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	result := <-c
-	fmt.Println(result)
-	fmt.Println(<-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
 
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- true
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	c <- person + " is sexy"
 }
